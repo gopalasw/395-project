993d0ac6ab5bf9d96186ea93057d595442436f73
gopalasw <gopalasw@grinnell.edu>
2016-10-31 20:50:55 -0500

    updated grammar

--- Grammar/Grammar.hs
+++ Grammar/Grammar.hs
@@ -3,24 +3,48 @@ module Grammar.Grammar where
 data Board = Board { a :: Player,
                      b :: Player,
                      weather :: [Card],
-                     roundScore :: (Int, Int) }
+                     roundScore :: (Int, Int)
+                     isATurn :: Bool }
+                   deriving (Show)
 
 data Player = Player { cardsInHand :: [Card],
                        cardsLeft :: [Card], 
                        cardsOnBoard :: [Card],
                        usedCards :: [Card],
-                       score :: [Int],
-                       leaderAbility :: Ability,
-                       countryAbility :: Ability }
+                       score :: Int,
+                       leader :: Card,
+                       country :: Country }
+                     deriving (Show)
 
+data Card =
+    CWeather Name [Row]
+  | CUnit Name Row Unit
+  | CLeader Leader
+  | CPass
+  deriving (Show)
 
-data Card = CWeather Name [Row] | CUnit Name Row Unit
 data Unit = Unit { ability :: Ability,
                    damage :: Int }
+                 deriving (Show)
+
+data Leader =
+    SteelForged  -- Scorch Siege if enemies Siege strength is 10 or higher 
+  | Siegemaster -- Horn on Siege row
+  | NorthCommander -- Clear any Weather effects
+  | KingTemeria -- Pick a Fog card from your deck and play it immediately
+  | Relentless -- Draw card from opp discard pile
+  | WhiteFlame -- Cancel opp Leader ability
+  | EmperorNilfgaard -- Look at 3 random cards of opp hand
+  | ImperialMajesty -- Pick a rain card from deck and play immediately
+  deriving (Show)
+
+data Country =
+    Nothern -- Draw extra card from deck after you win a round
+  | Nilfgaard -- Win the game if it is a draw
 
 data Ability =
     AMoraleBoost -- adds +1 to units in a single row, except for this card.
-  | AScorch -- if opp cc units row >= 10, destroys strongest unit in that row
+  | AScorch Row -- if opp cc units row >= 10, destroy strongest unit in row
   | ASpy -- puts card in opp board, draw 2 cards
   | AHero -- immune to abilities/special effects
   | ATightBond Name -- if beside same name card, strength of same name cards x2
@@ -32,4 +56,4 @@ data Ability =
 
 type Name = String -- Name of cards/countries/leaders
 type Row = Int -- Rows that the card can be played on/affects
-type Deck = (Name, [Card]) -- (Country name, relevant cards)
+type Deck = (Country, [Card]) -- (Country name, relevant cards)
