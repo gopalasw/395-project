e24f1284f49cb735da2e2b614aeb395a11183370
Jerry Chen <chenjian@grinnell.edu>
2016-11-02 22:34:27 -0500

    drawcards updated, win condition updated

--- Game/Basics.hs
+++ Game/Basics.hs
@@ -1,6 +1,10 @@
 module Game.Basics where
 
 import Grammar.Grammar
+--import Data.Random.Extras
+import Game.Cards
+import Data.List
+import System.Random
 
 main = undefined
 
@@ -8,8 +12,8 @@ main = undefined
 init :: (Country, Country) -> (Card, Card) -> Board 
 init (c1, c2) (l1, l2) = Board
   {
-    a          = genPlayer (drawCard c1) c1 l1,    
-    b          = genPlayer (drawCard c2) c2 l2,
+    a          = genPlayer (drawCards c1 10 allCards) c1 l1,    
+    b          = genPlayer (drawCards c2) c2 l2,
     weather    = [],
     roundScore = (0, 0),
     isATurn    = True
@@ -26,10 +30,35 @@ genPlayer (drew, left) c l =
     leader       = l,
     country      = c }
 
+type cardsToDraw = Int
 
--- fst: cards drew; snd: cards left
-drawCard :: Country ->  ([Card], [Card])
-drawCard = undefined
+drawCards :: Country -> Int -> [Card] -> Maybe ([Card], [Card])
+drawCards _ _ [] = Nothing
+drawCards _ 0 ls = Just ([], ls)
+drawCards c i ls = 
+  case tuple of
+  (Just card, remains) -> 
+    case (drawCards c (i - 1) remains) of
+      Just (dealt, left) -> Just (card:dealt, left)
+      Nothing            -> Nothing
+  otherwise            -> Nothing
+  where
+    tuple = drawCard ls 
+
+drawCard :: [Card] -> (Maybe Card, [Card])
+drawCard cards = do 
+  ran :: Int  <- randomRIO (0, length cards) :: IO Int
+  (getCard ran cards, removeCard ran cards)
+
+getCard :: Int -> [Card] -> Maybe Card
+getCard _ []     = Nothing
+getCard 0 (x:xs) = Just x
+getCard i (x:xs) = getCard (i - 1) xs
+
+removeCard :: Int -> [Card] -> [Card]
+removeCard _ []     = []
+removeCard 0 (x:xs) = xs
+removeCard i (x:xs) = x : (removeCard (i - 1) xs)
 
 swapCard :: Board -> Board 
 swapCard = undefined
@@ -51,13 +80,18 @@ evaluate currentB@(Board p1 p2 _ _ pTurn) =
     isATurn    = not pTurn
   }
   where
-    totalDamage p = foldl (+) 0 $  map damage $ getUnits (cardsOnBoard p)
+    totalDamage p = getTotalDamage (cardsOnBoard p)
 -- TODO
 -- This is a minimal version of evaluate, we only cares about damage for now
 
-getUnits :: [Card] -> [Unit]
-getUnits ls = 
-   (filter (\x -> x == CUnit) ls)
+getTotalDamage :: [Card] -> Int 
+getTotalDamage ls = 
+   foldl (+) 0 (map getDamage ls)
+
+getDamage :: Card -> Int
+getDamage (CUnit _ _ _ d) = d 
+getDamage _               = 0
+
 -- TODO
 -- Finsih getUnits. It filters throught a list of cardOnBoard and returns 
 -- all CUnit Cards.
@@ -74,10 +108,12 @@ roundOver (Board p1 p2 _ _ _) =
 -- checks if game is over by checking if player a or b has won
 gameOver :: Board -> Bool
 gameOver (Board p1 p2 _ _ _) =
+  ((fst lengths == 2) && (fst scores == 0)) ||
+  ((snd lengths == 2) && (snd scores == 0)) ||
   fst scores == 2 ||
   snd scores == 2 || 
-  ((fst lengths) == (snd lengths)) && 
-  (fst lengths == 3)
+  (((fst lengths) == (snd lengths)) && 
+  (fst lengths == 3))
   where
     lengths :: (Int, Int)
     lengths = ((length $ score $ p1), (length $ score $ p2)) 
