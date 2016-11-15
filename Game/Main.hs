a0d17a523d12b82dfca2f87a2cda48c955db9a88
Jerry Chen <chenjian@grinnell.edu>
2016-11-14 22:22:15 -0600

    game ends

--- Game/Main.hs
+++ Game/Main.hs
@@ -6,7 +6,7 @@ import Game.Init
 import Game.Round
 import Grammar.Grammar
 import Grammar.PrettyPrint
-
+import Grammar.Board
 import Control.Applicative
 import System.Random
 import Data.Time.Clock
@@ -17,8 +17,35 @@ main = do
   board <- roundSeq $ roundSeq $ pure board
   -- Is the game over? If so, then another round.
   -- board <- roundSeq $ pure board
+  if (gameOver board) 
+  then 
+    putStrLn $ gameEnd board 
+  else do
+    board <- roundSeq $ pure board
+    putStrLn $ gameEnd board 
+  
   putStrLn $ prettyPrintBoard board
+  
   where
     seed :: UTCTime -> StdGen
     seed t = mkStdGen $ floor $ utctDayTime t
     board t = initBoard (seed t) (Northern, Northern) ((CLeader Relentless), (CLeader Canceled))
+
+gameEnd :: Board -> String 
+gameEnd board =
+  case evalGame board of
+    (True,  False) -> "Game Over, the winner is player 1"
+    (False, True)  -> "Game Over, the winner is player 2"
+    (False, False) -> "Game Over, no one wins in this game"
+    (True,  True)  -> error "Game error: two winners"
+
+
+gameOver :: Board -> Bool
+gameOver (Board p1 p2 _ _ _ _) =
+  (length (lives p1)) == (length (lives p2)) &&
+  (length (lives p1)) == 3
+
+evalGame :: Board -> (Bool, Bool)
+evalGame (Board p1 p2 _ _ _ _) = 
+  (((foldl (+) 0 (lives p1)) == 2), 
+   ((foldl (+) 0 (lives p2)) == 2))
